874818192b722bdb1d520857538e46877d00c6c5
Puja Bhoite <pujabhoite25@gmail.com>
2019-07-28 10:48:41 +0530

    get automatic location info

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
+
 var app = angular.module('riverbook', []);
-app.controller('riverinfoController', function($scope) {
+app.run(function($rootScope) {
+    $rootScope.showLoginForm = true;
+    $rootScope.showRiverForm = false;
+});
+
+app.controller('LoginController', function($scope, $rootScope) {
+	$scope.rbusername;
+	$scope.rbpassword;
+	var msg;
+	
+	$scope.userLogin = function(){        
+		localStorage.setItem("rbusername", $scope.rbusername);
+		//window.location = "views/riverinfo.html";	
+        $rootScope.showLoginForm = false;
+        $rootScope.showRiverForm = true;
+	}
+});
+
+app.controller('RiverinfoController', function($scope) {
 	$scope.rbriverName;
 	$scope.rbriverLocation;
 	var msg;
@@ -20,9 +39,8 @@ app.controller('riverinfoController', function($scope) {
     }
 
     $scope.showPosition = function(position) {
-        alert(position.coords.latitude);
         $scope.rbriverLocationAuto = "Latitude: " + position.coords.latitude + 
-        "<br>Longitude: " + position.coords.longitude;
-        $scope.rbriverLocationAuto = "yes";
+        "<br>Longitude: " + position.coords.longitude;       
     }
 });
+
--- js/loginController.js
+++ js/loginController.js
@@ -1,11 +1,12 @@
 var app = angular.module('riverbook', []);
-app.controller('loginController', function($scope) {
+app.controller('loginController', function($scope, $rootScope) {
 	$scope.rbusername;
 	$scope.rbpassword;
 	var msg;
 	
 	$scope.userLogin = function(){
 		localStorage.setItem("rbusername", $scope.rbusername);
-		window.location = "views/riverinfo.html";		
+		//window.location = "views/riverinfo.html";		
+        $rootScope.showRiverForm = true;
 	}
 });
