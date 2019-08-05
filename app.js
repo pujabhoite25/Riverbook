b8607027bb9bee8a3090986f00aa50899f302a2a
Puja Bhoite <pujabhoite25@gmail.com>
2019-08-05 22:23:01 +0530

    Rendered map on riverinfo form and able to show a circled area for user selected area

--- app.js
+++ app.js
@@ -23,8 +23,50 @@ app.controller('RiverinfoController', function($scope, $sce, $http) {
 	$scope.rbriverLocation;
 	var msg;
 	
+
+	var mymap = L.map('rbmap').setView([51.505, -0.09], 13);
+
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox.streets'
+	}).addTo(mymap);
+
+	L.marker([51.5, -0.09]).addTo(mymap);
+
+	L.circle([51.508, -0.11], {
+		color: 'red',
+		fillColor: '#f03',
+		fillOpacity: 0.5,
+		radius: 500
+	}).addTo(mymap);
+
+	L.polygon([
+		[51.509, -0.08],
+		[51.503, -0.06],
+		[51.51, -0.047]
+	]).addTo(mymap);
+
+	function onMapClick(e) {
+		alert("You clicked the map at " + e.latlng);
+		console.log(e);
+		L.circle([e.latlng.lat, e.latlng.lng], {
+			color: 'red',
+			fillColor: '#f03',
+			fillOpacity: 0.5,
+			radius: 500
+		}).addTo(mymap);
+		
+	}
+
+mymap.on('click', onMapClick);
+	
 	var getUserData = function(){
 		$scope.rbusername = localStorage.getItem("rbusername");
+		
+		
 	}();
     $scope.allRivers = {
 	"rivers": [{
